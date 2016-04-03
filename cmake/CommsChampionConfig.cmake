44278ae0ab42d857c10bbc6c60e827b5880fbda9
Alex Robenko <arobenko@gmail.com>
2016-04-04 07:29:52 +1000

    Added CMake script for other projects.

---
+++ cmake/CommsChampionConfig.cmake
@@ -0,0 +1,39 @@
+# - Find CommsChampion
+# Find the CommsChampion includes and libraries
+# 
+# Output is provided using the following variables
+#  CC_COMMS_FOUND - Found COMMS library, use CC_INCLUDE_DIRS for includes
+#  CC_COMMS_CHAMPION_FOUND - Found CommsChampion, use CC_INCLUDE_DIRS, CC_PLUGIN_LIBRARIES, CC_PLUGIN_DIR)
+#  CC_INCLUDE_DIRS - Where to find all the headers
+#  CC_PLUGIN_LIBRARIES - Libraries to link against when building a plugin
+#  CC_PLUGIN_LIBRARY_DIRS - Where to find the libraries required to build plugin.
+#  CC_PLUGIN_DIR - Directory where to install newly built plugin
+
+get_filename_component (CC_INSTALL_PATH ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
+
+find_path(CC_INCLUDE_DIR NAMES comms/comms.h PATHS "${CC_INSTALL_PATH}" PATH_SUFFIXES include)
+find_library(CC_PLUGIN_LIBRARY  NAMES "comms_champion" PATHS "${CC_INSTALL_PATH}" PATH_SUFFIXES lib)
+find_library(CC_NULL_SOCK_LIBRARY  NAMES "null_socket" PATHS "${CC_INSTALL_PATH}" PATH_SUFFIXES plugin)
+
+if (CC_PLUGIN_LIBRARY)
+    get_filename_component  (CC_PLUGIN_LIBRARY_DIR ${CC_PLUGIN_LIBRARY} DIRECTORY)
+endif ()
+
+if (CC_NULL_SOCK_LIBRARY)
+    get_filename_component  (CC_PLUGIN_DIR ${CC_NULL_SOCK_LIBRARY} DIRECTORY)
+endif ()
+
+#include(FindPackageHandleStandardArgs.cmake)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CC_COMMS REQUIRED_VARS CC_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CC_COMMS_CHAMPION REQUIRED_VARS 
+    CC_INCLUDE_DIR CC_PLUGIN_LIBRARY_DIR CC_PLUGIN_LIBRARY CC_PLUGIN_DIR)
+    
+if (CC_COMMS_FOUND)
+    set (CC_INCLUDE_DIRS ${CC_INCLUDE_DIR})
+endif ()
+
+if (CC_COMMS_CHAMPION_FOUND)
+    set (CC_PLUGIN_LIBRARIES ${CC_PLUGIN_LIBRARY})
+    set (CC_PLUGIN_LIBRARY_DIRS ${CC_PLUGIN_LIBRARY_DIR})
+endif ()    
+

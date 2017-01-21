5c99e6cce35bdfcba8706274ac606ddd050fe8ae
Alex Robenko <arobenko@gmail.com>
2017-01-21 18:31:05 +1000

    Creating startup script for comms_dump app.

--- cmake/GenWinAppStartBat.cmake
+++ cmake/GenWinAppStartBat.cmake
@@ -9,5 +9,4 @@
 set (bat_file_path "${OUTPUT_DIR}/${APP_NAME}.bat")
 file (WRITE ${bat_file_path} "set PATH=${EXTRA_PATH};%PATH%\n")
 file (APPEND ${bat_file_path} "set app=%~dp0\\${APP_NAME}.exe\n")
-file (APPEND ${bat_file_path} "start %app% %*\n")
-file (APPEND ${bat_file_path} "exit\n")
+file (APPEND ${bat_file_path} "call %app% %*\n")

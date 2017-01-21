ce4781a6714b06a6bde7c09a95d37882e21d677e
Alex Robenko <arobenko@gmail.com>
2017-01-21 18:35:38 +1000

    Removing commands echo from Windows app start script.

--- cmake/GenWinAppStartBat.cmake
+++ cmake/GenWinAppStartBat.cmake
@@ -7,6 +7,6 @@
 # OUTPUT_DIR
 
 set (bat_file_path "${OUTPUT_DIR}/${APP_NAME}.bat")
-file (WRITE ${bat_file_path} "set PATH=${EXTRA_PATH};%PATH%\n")
-file (APPEND ${bat_file_path} "set app=%~dp0\\${APP_NAME}.exe\n")
-file (APPEND ${bat_file_path} "call %app% %*\n")
+file (WRITE ${bat_file_path} "@set PATH=${EXTRA_PATH};%PATH%\n")
+file (APPEND ${bat_file_path} "@set app=%~dp0\\${APP_NAME}.exe\n")
+file (APPEND ${bat_file_path} "@call %app% %*\n")

e0f488f3ec21cd19842226edd11ad4053849e831
Alex Robenko <arobenko@gmail.com>
2017-05-30 17:13:31 +1000

    More cosmtetic changes to appveyor build.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,14 +1,13 @@
-if NOT "%TOOLCHAIN%"=="msvc14" (
+IF NOT "%TOOLCHAIN%"=="msvc14" (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
 )
 
-if "%PLATFORM%"=="x86" (
+IF "%PLATFORM%"=="x86" (
     echo Performing x86 build
     call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
     set QTDIR="C:\Qt\5.7\msvc2015"
-)
-else (
+) ELSE (
     echo Performing amd64 build
     call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
     set QTDIR="C:\Qt\5.7\msvc2015_64"    

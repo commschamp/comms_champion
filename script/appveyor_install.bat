f13077f210c3704514de78ea99327bc123d996f7
Alex Robenko <arobenko@gmail.com>
2017-05-30 16:59:16 +1000

    String trying to start appveyor build.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,9 +1,9 @@
-if NOT %1=="msvc14" (
+if NOT "%TOOLCHAIN%"=="msvc14" (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
 )
 
-if %2=="x86" (
+if "%PLATFORM%"=="x86" (
     call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
     set QTDIR="C:\Qt\5.7\msvc2015"
 )

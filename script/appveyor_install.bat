141cbd43efa175ea775e8606a981a33bfdeda913
Alex Robenko <arobenko@gmail.com>
2018-05-18 20:21:56 +1000

    Update qt version in appveyor.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -6,10 +6,10 @@ IF NOT "%TOOLCHAIN%"=="msvc14" (
 IF "%PLATFORM%"=="x86" (
     echo Performing x86 build
     call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-    set QTDIR="C:\Qt\5.7\msvc2015"
+    set QTDIR="C:\Qt\5.9.5\msvc2015"
 ) ELSE (
     echo Performing amd64 build
     call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-    set QTDIR="C:\Qt\5.7\msvc2015_64"    
+    set QTDIR="C:\Qt\5.9.5\msvc2015_64"    
 )
 

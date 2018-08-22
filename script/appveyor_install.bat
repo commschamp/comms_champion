ab5497b7da149d59fb28362a7177804886ca1035
Alex Robenko <arobenko@gmail.com>
2018-08-22 11:21:46 +1000

    Updating appveyor build.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,20 +1,22 @@
-IF "%TOOLCHAIN%"=="msvc14" (
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
+    set TOOLCHAIN="msvc14"
     IF "%PLATFORM%"=="x86" (
-        echo Performing x86 build
+        echo Performing x86 build in VS2015
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
         set QTDIR="C:\Qt\5.6.3\msvc2015"
     ) ELSE (
-        echo Performing amd64 build
+        echo Performing amd64 build in VS2015
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
         set QTDIR="C:\Qt\5.6.3\msvc2015_64"    
     )
-) ELSE IF "%TOOLCHAIN%"=="msvc15" (
+) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
+    set TOOLCHAIN="msvc15"
     IF "%PLATFORM%"=="x86" (
-        echo Performing x86 build
+        echo Performing x86 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
         set QTDIR="C:\Qt\5.6.3\msvc2015"
     ) ELSE (
-        echo Performing amd64 build
+        echo Performing amd64 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
         set QTDIR="C:\Qt\5.6.3\msvc2015_64"    
     )

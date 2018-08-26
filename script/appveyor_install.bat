ec9e21fc421c8084e3af36fa0930e1c6392e5bc4
Alex Robenko <arobenko@gmail.com>
2018-08-26 13:45:14 +1000

    Fixing build with latest version of Qt5.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,24 +1,24 @@
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
-    set TOOLCHAIN="msvc14"
+    set TOOLCHAIN=msvc14
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2015
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-        set QTDIR="C:\Qt\5.6.3\msvc2015"
+        set QTDIR="C:\Qt\%QT_VER%\msvc2015"
     ) ELSE (
         echo Performing amd64 build in VS2015
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-        set QTDIR="C:\Qt\5.6.3\msvc2015_64"    
+        set QTDIR="C:\Qt\%QT_VER%\msvc2015_64"    
     )
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
-    set TOOLCHAIN="msvc15"
+    set TOOLCHAIN=msvc15
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
-        set QTDIR="C:\Qt\5.6.3\msvc2015"
+        set QTDIR="C:\Qt\%QT_VER%\msvc2015"
     ) ELSE (
         echo Performing amd64 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-        set QTDIR="C:\Qt\5.6.3\msvc2015_64"    
+        set QTDIR="C:\Qt\%QT_VER%\msvc2015_64"    
     )
 ) ELSE (
     echo Toolchain %TOOLCHAIN% is not supported

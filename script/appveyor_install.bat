26a741827d05f28472273392e534c98b5fe5b4e4
Alex Robenko <arobenko@gmail.com>
2018-09-20 08:19:16 +1000

    Updating appveyor configuration.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,5 +1,6 @@
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     set TOOLCHAIN=msvc14
+    set QT_SUBDIR=msvc2015
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2015
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
@@ -9,6 +10,7 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     )
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     set TOOLCHAIN=msvc15
+    set QT_SUBDIR=msvc2015
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
@@ -27,9 +29,10 @@ IF "%PLATFORM%"=="x64" (
     set QT_DIR_SUFFIX=_64
 )    
 
-set QTDIR=%QTDIR_PREFIX%\%TOOLCHAIN%%QT_DIR_SUFFIX%
+set QTDIR=%QTDIR_PREFIX%\%QT_SUBDIR%%QT_DIR_SUFFIX%
 IF NOT EXIST %QTDIR% (
-    set QTDIR=%QTDIR_PREFIX%\msvc215%QT_DIR_SUFFIX%
+    echo WARNING: %QTDIR% does not exist!!!
+    set QTDIR=%QTDIR_PREFIX%\msvc2015%QT_DIR_SUFFIX%
 )
 
 echo Using Qt5 from %QTDIR%

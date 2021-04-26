39c41c97758b0db2653fa90edf91bcab092929e4
Alex Robenko <arobenko@gmail.com>
2021-04-27 08:36:00 +1000

    Updated copyright notice to include year 2021.

--- comms_champion/lib/include/comms_champion/field_wrapper/FloatValueWrapper.h
+++ comms_champion/lib/include/comms_champion/field_wrapper/FloatValueWrapper.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2014 - 2020 (C). Alex Robenko. All rights reserved.
+// Copyright 2014 - 2021 (C). Alex Robenko. All rights reserved.
 //
 
 // This file is free software: you can redistribute it and/or modify

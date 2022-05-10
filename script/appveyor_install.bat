4095d74a395b00f3b27f65c1f0466f3970def1a7
Alex Robenko <arobenko@gmail.com>
2022-05-11 08:00:25 +1000

    Updating appveyor configuration.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -23,8 +23,8 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
     set TOOLCHAIN=msvc16
-    set QT_SUBDIR=msvc2017
-    set QT_VER=5.14
+    set QT_SUBDIR=msvc2019
+    set QT_VER=5.15.2
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2019
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
@@ -32,6 +32,17 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
         echo Performing amd64 build in VS2019
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
     )
+) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2022" (
+    set TOOLCHAIN=msvc17
+    set QT_SUBDIR=msvc2019
+    set QT_VER=5.15.2
+    IF "%PLATFORM%"=="x86" (
+        echo Performing x86 build in VS2022
+        call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+    ) ELSE (
+        echo Performing amd64 build in VS2022
+        call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    )  
 ) ELSE (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
